47d574d44910cfa6434f32f109dbd040603fe293
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2017-10-24 20:12:06 +0700

    fix conflict

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,96 +3,63 @@ import styled from 'styled-components';
 import squares from '../../assets/squares.png';
 import squaresParticle from '../../assets/squares-particel.svg';
 import Footer from '../../components/Footer';
-<<<<<<< e894fa3827a994ed7350da8969dcf08b1972078b
 import Navigation from '../../components/Navigation';
-=======
-import Content from './Content';
->>>>>>> change home index
-import { LOGIN_PATH } from '../../common/routing';
+import {LOGIN_PATH} from '../../common/routing';
+import showcaseLogo from '../../assets/logo-showcase.png';
 
 class Home extends Component {
   render() {
     return (
-<<<<<<< e894fa3827a994ed7350da8969dcf08b1972078b
-      <Body>
-        <Title>
-          <img src={squares} className="squares" alt="square logo" />
-          <span>Pekan Ristek 2017</span>
-        </Title>
-        <Subtitle>IT Strikes Back</Subtitle>
-        <Button onClick={() => this.props.history.push(LOGIN_PATH)}>Login</Button>
-      </Body>
-=======
       <Wrapper>
-        <Container>
+        <Navigation />
           <Body>
-            <Section>
-              <Group>
-                <Section>
-                  <Layer>
-                    <Level01>
-                      <span className="title">Pekan Ristek 2017</span>
-                      <p className="p-about">merupakan ajang tahunan yang diselenggarakan oleh Ristek Fasilkom UI, bertujuan untuk meningkatkan minat dan ketertarikan terhadap dunia IT. Tahun ini, Pekan Ristek membawakan tema "IT Strikes Back"</p>
-                    </Level01>
-                    <Level02>
-                    </Level02>
-                  </Layer>
-                </Section>
-              </Group>
-            </Section>
-            <Content />
+            <div className="header-image">
+              <div className="background-logo">
+                <img src={squaresParticle} alt="" />
+              </div>
+            </div>
+            <div className="header-text">
+              <span>Pekan Ristek 2017</span>
+              <div className="text">
+                merupakan ajang tahunan yang diselenggarakan oleh Ristek Fasilkom UI, bertujuan untuk meningkatkan minat dan ketertarikan terhadap dunia IT. Tahun ini, Pekan Ristek membawakan tema "IT Strikes Back"
+              </div>
+            </div>
+           <div className="showcase-section">
+           <div className="showcase-header-logo">
+            {/* @TODO change to svg and fix the dimension */}
+              <img src={squares} />
+            </div>
+            <div className="showcase-title">
+              <span>Showcase</span>
+            </div>
+            <div className="showcase-text">
+            </div>
+           </div>
           </Body>
-          <Footer />
-        </Container>
       </Wrapper>
->>>>>>> change home index
     );
   }
 }
 
 export default Home;
 
-<<<<<<< e894fa3827a994ed7350da8969dcf08b1972078b
-=======
 const Wrapper = styled.div`
-  max-width: 100%;
-  min-height: 100vh;
-  overflow-y: hidden;
-  overflow-x: hidden;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  background: linear-gradient(-20deg, #fff 0%, #f5f5f5 100%);
-
-  img {
-    user-select: none;
-  }
-`;
-
-const Container = styled.div`
   width: 100%;
-  min-height: 100vh;
+  padding: 0 8rem;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  z-index: 1;
+  background: ${props => props.theme.color.white};
 `;
 
->>>>>>> change home index
 const Body = styled.div`
   display: flex;
-  margin: 2rem 0 0 0;
+  margin: 5rem 0 0 0;
   flex-direction: column;
   align-items: center;
-<<<<<<< e894fa3827a994ed7350da8969dcf08b1972078b
-  justify-content: center;
-=======
-  min-height: 90vh;
-  margin-bottom: 4.5rem;
+  justify-content: flex-start;
   width: 100%;
->>>>>>> change home index
   .logoContainer {
     width: 30rem;
     display: flex;
@@ -104,99 +71,86 @@ const Body = styled.div`
       height: 5rem;
     }
   }
-`;
-
-const Section = styled.section`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-  overflow-x: hidden;
-  overflow-y: auto;
-`;
-
-const Group = styled.div`
-  position: relative;
-  height: 100vh;
-  width: 100%;
-  -webkit-transform-style: preserve-3d;
-  transform-style: preserve-3d;
-`;
-
-const Layer = styled.div`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
-  -webkit-transform-origin-x: 100%;
-  transform-origin-x: 100%;
-`;
-
-const Level01 = styled.div`
-  display: flex;
-  flex-direction: column;
-  transform: translateZ(0);
-  font-family: ${props => props.theme.font.pekanRistek};
-  font-size: 2.5rem;
-  height: 100vh;
-  background-image: url(${squaresParticle});
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: 75% 75%;
-  background-attachment: ;
-  .title {
-    text-align: right;
-    padding-right: 5rem;
-    margin-top: 8rem;
-    padding-left: 2rem;
-  }
-  p,
-  p-about,
-  p-showcase {
-    font-family: ${props => props.theme.font.jaapokki};
-    font-size: 1rem;
-    word-break: break-all;
-  }
-  p-about {
-    padding-left: 10rem;
-  }
-`;
-
-const Level02 = styled.div`
-  display: flex;
-  flex-direction: column;
-  transform: translateZ(-1) scale(0.6);
-  .logo-particle {
-    position: fixed;
+  .header-image {
     width: 100%;
-    height: 100%;
+    height: 60vh;
+    position: relative;
+    .background-logo {
+      position: absolute;
+      width: 100%;
+      height: 60vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      img {
+        height: 25rem;
+        width: 25rem;
+        object-fit: contain;
+      }
+    }
+  }
+  .header-text {
+    width: 100%;
+    height: 60vh;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    span {
+      font-size: 1.8rem;
+      font-family: ${props => props.theme.font.pekanRistek};
+      padding-right: 14rem;
+    }
+    .text {
+      width: 35%;
+      font-size: 1rem;
+      padding: 0 2rem 0 2rem;
+      margin: 4rem 4.5rem 0 0;
+      font-family: ${props => props.theme.font.jaapokki};
+    }
+  }
+  .showcase-section {
+    margin-top: 10rem;
+    .showcase-header {
+      width: 100%;
+      height: 60vh;
+      position: relative;
+      display: flex;
+      flex-direction: row;
+      align-items: flex-start;
+  
+      .showcase-header-logo {
+        width: 100%;
+        height: inherit;
+        position: absolute;
+        display: flex;
+        align-items: flex-start;
+  
+        img {
+          height: 5rem;
+          width: 3rem;
+          object-fit: contain;
+        }
+      }
+    }
+    .showcase-title {
+      width: 100%;
+      height: 60vh;
+      position: relative;
+      display: flex;
+      flex-direction: row;
+      align-items: flex-start;
+      span {
+        font-family: ${props => props.theme.font.jaapokki};
+        font-size: 2.5rem;
+      }
+    }
+    .showcase-text {
+      display: flex;
+      align-items: flex-start;
+      width: 100%%;
+      font-size: 1rem;
+      font-family: ${props => props.theme.font.jaapokki};
+    }
   }
 `;
-
-const Level03 = styled.div`
-  transform: translateZ(-2) scale(0.6);
-  display: flex;
-  flex-direction: column;
-`;
-
-const Level04 = styled.div`
-  transform: translateZ(-3) scale(0.6);
-`;
-
-// const Subtitle = styled.span`
-//   font-family: ${props => props.theme.font.jaapokki};
-//   font-size: 2rem;
-// `;
-
-// const Button = styled.button`
-//   background: #3498db;
-//   border: none;
-//   border-radius: 1rem;
-//   color: ${props => props.theme.color.white};
-//   padding: 1rem 2rem;
-//   text-decoration: none;
-//   &:hover {
-//     cursor: pointer;
-//   }
-// `;
