56de0aee84409c34446f660590a912c3b4d1c204
albertusangga <albertusangga.98@gmail.com>
2017-10-28 18:49:25 +0700

    Fix poi config to resolve npm run dev bug

--- poi.config.js
+++ poi.config.js
@@ -1,16 +1,29 @@
 const LOCAL_URL = 'http://localhost:4000/pekan-ristek/';
 const PRODUCTION_URL = 'http://ristek.cs.ui.ac.id/pekan-ristek/';
 
-module.exports = (options, req) => ({
-  webpack(config) {
-    config.output.publicPath = PRODUCTION_URL;
-    return config;
-  },
-  html: {
-    title: 'Pekan Ristek 2017',
-  },
-  env: {
-    NODE_ENV: process.env.NODE_ENV || 'development',
-  },
-  port: 4004,
-});
+module.exports = (options, req) => {
+  if (process.env.NODE === 'production') {
+    return {
+      webpack(config) {
+        config.output.publicPath = PRODUCTION_URL;
+        return config;
+      },
+      html: {
+        title: 'Pekan Ristek 2017',
+      },
+      env: {
+        NODE_ENV: process.env.NODE_ENV || 'development',
+      },
+      port: 4004,
+    };
+  }
+  return {
+    html: {
+      title: 'Pekan Ristek 2017',
+    },
+    env: {
+      NODE_ENV: process.env.NODE_ENV || 'development',
+    },
+    port: 4004,
+  };
+};
